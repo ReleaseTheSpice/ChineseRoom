a860c3f29e0c6b703e5d3e1aad1c4fff179a4265
Kotailri <willc8810@gmail.com>
2022-11-28 22:29:59 -0800

    added terminal canvas to terminal object

--- Assets/Prefabs/Terminal/StartTerminalScript.cs
+++ Assets/Prefabs/Terminal/StartTerminalScript.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class StartTerminalScript : MonoBehaviour
 {
+    public GameObject TerminalUI;
+
     bool touchingTerminal = false;
     private void Start()
     {
-
+        TerminalUI.SetActive(false);
     }
 
     private void Update()
     {
-        if (touchingTerminal)
+        if (Input.GetKeyDown(KeyCode.E) && touchingTerminal)
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            if (TerminalUI.activeInHierarchy)
             {
-                Debug.Log("Start Terminal");
+                Debug.Log("Close Terminal");
+                TerminalUI.SetActive(false);
             }
+            else
+            {
+                Debug.Log("Open Terminal");
+                TerminalUI.SetActive(true);
+            }
+            
         }
     }
 
