f06f47b96ced2c66efffae7b83855015db32cd33
Hevano <evanohirabe@gmail.com>
2022-12-02 11:45:26 -0800

    Added doors that can be opened and closed

---
+++ Assets/Scripts/InteractableDoor.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableDoor : MonoBehaviour, ISelectableBehaviour
+{
+    [SerializeField]
+    private Transform doorLeft;
+    [SerializeField]
+    private Transform doorRight;
+
+    private Vector3 altDoorPosLeft;
+    private Vector3 altDoorPosRight;
+
+    private bool open = false;
+
+    private float elapsed = 0f;
+
+    public void clicked()
+    {
+        open = !open;
+        if(open){
+            altDoorPosLeft = doorLeft.position + new Vector3(0,0,-1);
+            altDoorPosRight = doorRight.position + new Vector3(0,0,1);
+            elapsed = 1.0f;
+        } else {
+            altDoorPosLeft = doorLeft.position + new Vector3(0,0,1);
+            altDoorPosRight = doorRight.position + new Vector3(0,0,-1);
+            elapsed = 1.0f;
+        }
+    }
+
+    public void Update(){
+        if(elapsed > 0){
+            doorLeft.position = Vector3.Lerp(altDoorPosLeft, doorLeft.position, elapsed);
+            doorRight.position = Vector3.Lerp(altDoorPosRight, doorRight.position, elapsed);
+
+            elapsed -= 0.001f;
+        }
+    }
+}
