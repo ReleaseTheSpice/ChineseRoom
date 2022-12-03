eb52149df1ff2385bf43da495afdd4272a12cf5a
mikesinclair66 <michaelkevin.sinclair@gmail.com>
2022-12-02 17:01:09 -0800

    Added backspace and enter functionality to UI

---
+++ Assets/Scripts/EmojiKeyboard.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EmojiKeyboard : MonoBehaviour
+{
+    private TMP_Text mtextComponent;
+    private bool mkeyDown = false;
+
+    private void Awake()
+    {
+        mtextComponent = GetComponent<TMP_Text>();
+    }
+
+    void Update()
+    {
+        if(!mkeyDown)
+        {
+            if(Input.GetKeyDown(KeyCode.Backspace))
+            {
+                mtextComponent.text = mtextComponent.text.Substring(0,
+                    mtextComponent.text.Length - GetLastTagSize(mtextComponent.text));
+                mkeyDown = true;
+            }
+            else if(Input.GetKeyDown(KeyCode.Return))
+            {
+                //EXTRACT INPUT VALUE HERE
+
+                mtextComponent.text = ">";
+                mkeyDown = true;
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Backspace) || Input.GetKeyUp(KeyCode.Return))
+                mkeyDown = false;
+    }
+
+    int GetLastTagSize(string value)
+    {
+        int lindex = value.Length - 1, size = 0;
+        do
+        {
+            lindex--;
+            size++;
+        } while (value[lindex] != '<');
+
+        return size + 1;
+    }
+}
